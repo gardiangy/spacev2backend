3157d13910dbbc6690e989d817c2f95030d13bc7
gardian <gardiangy@gmail.com>
2017-07-04 22:57:23 +0200

    notification websocket backend

---
+++ src/main/java/hu/voga/space/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package hu.voga.space.config;
+
+import hu.voga.space.handler.NotificationHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+
+
+@Configuration
+@EnableWebSocket
+@EnableScheduling
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    NotificationHandler notificationHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(notificationHandler, "/notification");
+    }
+}
